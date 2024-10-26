50909dbf4c836c503f7dc5f8901c0df1287b3378
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2024-10-26 22:09:02 +0600

    Make it responsive and change the filling color of second's clock

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ const minutesText = document.getElementById("minutes");
 const secondsText = document.getElementById("seconds");
 const periodText = document.getElementById("period");
 
+let isGreen = true;
+
 function updateClock() {
   const now = new Date();
   let hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  // Set AM/PM
   let period = hours >= 12 ? "PM" : "AM";
   periodText.textContent = period;
 
-  // Convert to 12-hour format
   if (hours > 12) {
     hours -= 12;
   }
@@ -29,24 +29,25 @@ function updateClock() {
     hours = 12;
   }
 
-  // Calculate degrees for rotation and fill
   const hoursRotation = (hours / 12) * 360;
   const minutesRotation = (minutes / 60) * 360;
   const secondsRotation = (seconds / 60) * 360;
 
-  // Update circles fill
+  if (seconds === 0) {
+    isGreen = !isGreen;
+  }
+  const secondsFill = isGreen ? "#04fc43" : "#fff";
+  const secondsEmpty = isGreen ? "#fff" : "#04fc43";
+
   hoursCircle.style.background = `conic-gradient(#ff2972 ${hoursRotation}deg, #fff ${hoursRotation}deg)`;
   minutesCircle.style.background = `conic-gradient(#fff ${minutesRotation}deg, #fee800 ${minutesRotation}deg)`;
-  secondsCircle.style.background = `conic-gradient(#04fc43 ${secondsRotation}deg, #fff ${secondsRotation}deg)`;
+  secondsCircle.style.background = `conic-gradient(${secondsFill} ${secondsRotation}deg, ${secondsEmpty} ${secondsRotation}deg)`;
 
-  // Update time text
   hoursText.textContent = String(hours).padStart(2, "0");
   minutesText.textContent = String(minutes).padStart(2, "0");
   secondsText.textContent = String(seconds).padStart(2, "0");
 }
 
-// Run the updateClock function every second
 setInterval(updateClock, 1000);
 
-// Initial call to display the clock immediately
 updateClock();
